4142a0914539ee479cec36e700e16c5b5435f9e5
Mathieu Hendey <m@mathieuhendey.com>
2019-03-21 16:24:07 +0000

    Fix index.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,4 @@
 export * from './app/client';
+export * from './interfaces/requestConfig';
+export * from './interfaces/workstreamCreateRequest';
+export * from './interfaces/workstreamCreateResponse';
