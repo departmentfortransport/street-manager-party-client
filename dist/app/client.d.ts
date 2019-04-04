f000aa560b03b72dfbbecb141b397f5480852c60
Ryan Fay <ryanf@kainos.com>
2019-04-04 13:34:41 +0100

    Updating party client for get workstream details

--- dist/app/client.d.ts
+++ dist/app/client.d.ts
@@ -1,6 +1,7 @@
 import { RequestConfig } from '../interfaces/requestConfig';
 import { WorkstreamCreateRequest } from '../interfaces/workstreamCreateRequest';
 import { WorkstreamCreateResponse } from '../interfaces/workstreamCreateResponse';
+import { WorkstreamResponse } from '../interfaces/workstreamResponse';
 export interface StreetManagerPartyClientConfig {
     baseURL: string;
     timeout?: number;
@@ -11,6 +12,7 @@ export declare class StreetManagerPartyClient {
     constructor(config: StreetManagerPartyClientConfig);
     isAvailable(): Promise<boolean>;
     createWorkstream(requestConfig: RequestConfig, organisationId: string, workstreamCreateRequest: WorkstreamCreateRequest): Promise<WorkstreamCreateResponse>;
+    getWorkstreamDetails(requestConfig: RequestConfig, organisationId: string, workstreamId: string): Promise<WorkstreamResponse>;
     private httpHandler;
     private handleError;
     private generateRequestConfig;
---
+++ dist/interfaces/workstreamResponse.d.ts
@@ -0,0 +1,8 @@
+export interface WorkstreamResponse {
+    prefix: string;
+    name: string;
+    description?: string;
+    contact_name?: string;
+    contact_number?: string;
+    contact_email?: string;
+}
