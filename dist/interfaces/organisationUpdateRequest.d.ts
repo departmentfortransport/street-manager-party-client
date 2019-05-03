3210fe9b70f7e75447a229819358bfbf05c6e102
Catherine Paul <c.paul@kainos.com>
2019-05-03 08:18:04 +0100

    SM-2199 - Add organisation update endpoint

---
+++ dist/interfaces/organisationUpdateRequest.d.ts
@@ -0,0 +1,28 @@
+export interface OrganisationUpdateRequest {
+    /** Max length 255 characters
+     * Alphanumeric characters only
+     */
+    address_line_one: string;
+    /** Max length 255 characters
+     * Alphanumeric characters only
+     */
+    address_line_two?: string;
+    /** Max length 255 characters
+     * Alphanumeric characters only
+     */
+    locality?: string;
+    /** Max length 255 characters
+     * Alphanumeric characters only
+     */
+    town?: string;
+    /** Max length 8 characters
+     * Alphanumeric characters only
+     */
+    postcode: string;
+    /** Max length 320 characters
+     * Must be valid email
+     */
+    email: string;
+    /** Max length 20 characters */
+    phone_number: string;
+}
--- src/app/client.ts
+++ src/app/client.ts
@@ -5,6 +5,7 @@ import { WorkstreamCreateRequest } from '../interfaces/workstreamCreateRequest'
 import { WorkstreamCreateResponse } from '../interfaces/workstreamCreateResponse'
 import { WorkstreamResponse } from '../interfaces/workstreamResponse'
 import { OrganisationResponse } from '../interfaces/organisationResponse'
+import { OrganisationUpdateRequest } from '../interfaces/organisationUpdateRequest'
 
 export interface StreetManagerPartyClientConfig {
   baseURL: string,
@@ -29,20 +30,24 @@ export class StreetManagerPartyClient {
     }
   }
 
-  public async getWorkstream(requestConfig: RequestConfig, organisationId: string, workstreamId: number): Promise<WorkstreamResponse> {
-    return this.httpHandler<WorkstreamResponse>(() => this.axios.get(`/organisations/${organisationId}/workstreams/${workstreamId}`, this.generateRequestConfig(requestConfig)))
+  public async getWorkstream(requestConfig: RequestConfig, swaCode: string, workstreamId: number): Promise<WorkstreamResponse> {
+    return this.httpHandler<WorkstreamResponse>(() => this.axios.get(`/organisations/${swaCode}/workstreams/${workstreamId}`, this.generateRequestConfig(requestConfig)))
   }
 
-  public async getOrganisation(requestConfig: RequestConfig, organisationId: string): Promise<OrganisationResponse> {
-    return this.httpHandler<OrganisationResponse>(() => this.axios.get(`/organisations/${organisationId}`, this.generateRequestConfig(requestConfig)))
+  public async getOrganisation(requestConfig: RequestConfig, swaCode: string): Promise<OrganisationResponse> {
+    return this.httpHandler<OrganisationResponse>(() => this.axios.get(`/organisations/${swaCode}`, this.generateRequestConfig(requestConfig)))
   }
 
-  public async getWorkstreams(requestConfig: RequestConfig, organisationId: string): Promise<WorkstreamResponse[]> {
-    return this.httpHandler<WorkstreamResponse[]>(() => this.axios.get(`/organisations/${organisationId}/workstreams`, this.generateRequestConfig(requestConfig)))
+  public async getWorkstreams(requestConfig: RequestConfig, swaCode: string): Promise<WorkstreamResponse[]> {
+    return this.httpHandler<WorkstreamResponse[]>(() => this.axios.get(`/organisations/${swaCode}/workstreams`, this.generateRequestConfig(requestConfig)))
   }
 
-  public async createWorkstream(requestConfig: RequestConfig, organisationId: string, workstreamCreateRequest: WorkstreamCreateRequest): Promise<WorkstreamCreateResponse> {
-    return this.httpHandler<WorkstreamCreateResponse>(() => this.axios.post(`/organisations/${organisationId}/workstreams`, workstreamCreateRequest, this.generateRequestConfig(requestConfig)))
+  public async createWorkstream(requestConfig: RequestConfig, swaCode: string, workstreamCreateRequest: WorkstreamCreateRequest): Promise<WorkstreamCreateResponse> {
+    return this.httpHandler<WorkstreamCreateResponse>(() => this.axios.post(`/organisations/${swaCode}/workstreams`, workstreamCreateRequest, this.generateRequestConfig(requestConfig)))
+  }
+
+  public async updateOrganisation(requestConfig: RequestConfig, swaCode: string, organisationUpdateRequest: OrganisationUpdateRequest): Promise<void> {
+    return this.httpHandler<void>(() => this.axios.put(`/organisations/${swaCode}`, organisationUpdateRequest, this.generateRequestConfig(requestConfig)))
   }
 
   private async httpHandler<T>(request: () => AxiosPromise<T>): Promise<T> {
