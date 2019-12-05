bef48dceb1f6355065bcaf1045cb3251d514223e
Cormac Curran <c.curran@kainos.com>
2019-12-05 16:29:59 +0000

    feature/SM-4177: As an Admin I want to record names of users when adding them to system
    update invite user endpoint to include first name and surname
    update invite admit endpoint to include first name and surname
    update create user endpoint to include first name and surname

--- dist/interfaces/inviteUserRequest.d.ts
+++ dist/interfaces/inviteUserRequest.d.ts
@@ -2,5 +2,9 @@ import { UserGroup } from './referenceTypes';
 export interface InviteUserRequest {
     /** Max length 50 characters */
     email: string;
+    /** Max length 50 characters */
+    first_name: string;
+    /** Max length 50 characters */
+    last_name: string;
     role?: UserGroup;
 }
