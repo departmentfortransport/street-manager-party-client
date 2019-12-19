9a4300b016604b8e0003b8e360b30dd3fe45acbd
shaung <s.ganley@kainos.com>
2019-12-19 11:42:35 +0000

    remove email from request as it can be retrieved from url

--- dist/interfaces/updateUserRolesRequest.d.ts
+++ dist/interfaces/updateUserRolesRequest.d.ts
@@ -1,6 +1,4 @@
 import { UserGroup } from './referenceTypes';
 export interface UpdateUserRolesRequest {
-    /** Max length 50 characters */
-    email: string;
     roles: UserGroup[];
 }
--- src/interfaces/updateUserRolesRequest.ts
+++ src/interfaces/updateUserRolesRequest.ts
@@ -1,7 +1,5 @@
 import { UserGroup } from './referenceTypes'
 
 export interface UpdateUserRolesRequest {
-  /** Max length 50 characters */
-  email: string
   roles: UserGroup[]
 }
